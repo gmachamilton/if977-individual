2cfcc3e942ce220b7db2af16f467ac28732f23ed
Gabriel Mac Hamilton Renaux Alves <gmhra@WINDOWS.CIN.UFPE.BR>
2018-03-14 18:22:51 -0300

    to subindo metodo gs

---
+++ metodo.js
@@ -0,0 +1,24 @@
+function Person() {
+    this.name = '';
+    this.age = '';
+    this.eyesColor = '';
+    this.body = '';
+
+    this.move = function() {
+        //
+    }
+
+    this.say = function() {
+        //      
+    }
+
+    this.see = function() {
+        //      
+    }
+
+}
+
+var lucas = new Person();
+
+console.log(typeof Person());
+console.log(typeof lucas);
